812ddd0599cc979e2f56c3222739af9c9f3f9f22
Brad <brad@viziworx.com>
2019-10-08 17:11:54 -0400

    Setup .map to iterate over array

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -7,13 +7,19 @@ import OperatorButton from "./OperatorButton";
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
-  const [operatorState, setOperatorState] = useState(operators);
-  
+  const [operatorState] = useState(operators);
+
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/
+        operatorState.map((button, index) => (
+          <OperatorButton key={index} button={button} />
+        ))
+       }
     </div>
   );
 };
+
+export default Operators;
