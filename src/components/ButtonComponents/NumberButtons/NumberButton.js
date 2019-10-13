7a94f8eed0c46153720f74621f309a33cad9a4b8
Brad <brad@viziworx.com>
2019-10-13 14:24:27 -0400

    added useState to new  displayValue

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -3,7 +3,7 @@ import React from "react";
 const NumberButton = props => {
   return (
     /* Display a button element rendering the data being passed down from the parent container on props */
-    <button className="numberButton">
+    <button className="numberButton" onClick={() => console.log(props.text)}>
       {props.text}
     </button>
   );
