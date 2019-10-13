b5cc3c157901960cd0c0652163f1fcc670e8a8a2
Brad <brad@viziworx.com>
2019-10-13 14:51:42 -0400

    Added addOperator props and updated onClick

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -4,7 +4,7 @@ const OperatorButton = props => {
   console.log(props);
   return (
     /* Display a button element rendering the data being passed down from the parent container on props */
-    <button className="operatorButton">
+    <button className="operatorButton" onClick={() => props.addOperator(props.operator.value)}>
       {props.operator.char}
     </button>
   );
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -5,7 +5,7 @@ import OperatorButton from "./OperatorButton";
 
 //Import your array data to from the provided data file
 
-const Operators = () => {
+const Operators = (props) => {
   // STEP 2 - add the imported data to state
   const [operatorState] = useState(operators);
 
@@ -14,7 +14,7 @@ const Operators = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
       it any props needed by the child component*/}
-        {operators.map(operator => <OperatorButton operator={operator} />)}
+        {operators.map(operator => <OperatorButton operator={operator} addOperator={props.addOperator} />)}
     </div>
   );
 };
